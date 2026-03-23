6cdb8024c0120a6c71f57b733850e6073d0f90c7
wahanePriyanshu <priyanshugwahane@.com>
2026-03-23 16:25:47 +0530

    Update API URL to EC2 IP and port 8081

--- frontend/revpay-frontend/src/environments/environment.ts
+++ frontend/revpay-frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 import { APP_ID } from "@angular/core";
 
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8081/api'
+ production: true,
+  // Change localhost to your EC2 IP and port to 8081
+  apiBaseUrl: 'http://13.60.88.223:8081/api' 
 };
